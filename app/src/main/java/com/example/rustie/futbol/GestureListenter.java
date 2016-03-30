6d2f3f5b9ed31211b64b72fec2ae9e4410902fca
rustielin <rustie117@gmail.com>
2016-03-30 16:59:34 -0700

    more updates

---
+++ app/src/main/java/com/example/rustie/futbol/GestureListenter.java
@@ -0,0 +1,9 @@
+package com.example.rustie.futbol;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+/**
+ * Created by rustie on 3/30/16.
+ */
+
--- app/src/main/java/com/example/rustie/futbol/MainActivity.java
+++ app/src/main/java/com/example/rustie/futbol/MainActivity.java
@@ -1,36 +1,83 @@
 package com.example.rustie.futbol;
 
+import android.gesture.Gesture;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public int goals = 0;
+    TextView goalCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        TextView goalCount = (TextView) findViewById(R.id.goals);
-        goalCount.setText("" + goals);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);
+
+        this.goalCount = (TextView) findViewById(R.id.goals);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 goals++;
+                goalCount.setText("" + goals);
             }
         });
-        goalCount.setText("" + goals);
 
+        final GestureDetector gdt =
+        final ImageView ball = (ImageView) findViewById(R.id.soccer_ball);
+        ball.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(final View view, final MotionEvent event) {
+                goals++;
+                goalCount.setText("" + goals);
+            }
+        });
+
+    }
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                goals++;
+                goalCount.setText("" + goals);
+                return false; // Right to left
+            }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                return false; // Left to right
+            }
+
+            if(e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                return false; // Bottom to top
+            }  else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                return false; // Top to bottom
+            }
+            return false;
+        }
     }
 
 }
